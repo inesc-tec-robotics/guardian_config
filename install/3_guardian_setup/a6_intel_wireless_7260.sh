66706cde44b401eb3e3e80592f1ae9546f07f09a
Carlos Costa <carloscosta.cmcc@gmail.com>
2015-06-13 20:22:33 +0100

    Added missing cd to wireless script.

--- install/3_guardian_setup/a6_intel_wireless_7260.sh
+++ install/3_guardian_setup/a6_intel_wireless_7260.sh
@@ -6,6 +6,7 @@ echo "##########################################################################
 
 cd ~
 mkdir drivers
+cd drivers
 wget https://wireless.wiki.kernel.org/_media/en/users/drivers/iwlwifi-7260-ucode-22.24.8.0.tgz
 tar zxvf iwlwifi-7260-ucode-22.24.8.0.tgz
 cd iwlwifi-7260-ucode-22.24.8.0
